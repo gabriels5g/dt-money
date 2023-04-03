a0c9c50faa93dc9f0603361fd19462e5c035fb24
gabriels5g <gabrielspxls@gmail.com>
2023-04-02 23:05:27 -0300

    styling modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,11 @@
+import Image from "next/image";
 import Modal from "react-modal";
+import Close from "../../assets/close.svg";
+import incomeImg from "../../assets/entrada.svg";
+import outcomeImg from "../../assets/saida.svg";
+import { Container, TransactionTypeContainer } from "./styles";
 
-interface NewTransactionModalPrrops {
+interface NewTransactionModalProps {
   isOpen: boolean;
 
   onRequesteClose: () => void;
@@ -9,10 +14,36 @@ interface NewTransactionModalPrrops {
 export function NewTransactionModal({
   isOpen,
   onRequesteClose,
-}: NewTransactionModalPrrops) {
+}: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequesteClose}>
-      <h2>cadastrar Transaction</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequesteClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button className="react-modal-close" onClick={onRequesteClose}>
+        <Image src={Close} width={24} height={24} alt={""} />
+      </button>
+      <Container>
+        <h2>Nova transação</h2>
+        <input type="text" placeholder="Descrição" />
+        <input type="number" placeholder="Preço" />
+
+        <input type="number" placeholder="Categoria" />
+        <TransactionTypeContainer>
+          <button type="button">
+            <Image src={incomeImg} width={24} height={24} alt="entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            {" "}
+            <Image src={outcomeImg} width={24} height={24} alt="entrada" />
+            <span>Saida</span>
+          </button>
+        </TransactionTypeContainer>
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   );
 }
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,62 @@
 import styled from "styled-components";
 
-styled;
+export const Container = styled.form`
+  h2 {
+    color: #e1e1e6;
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  input {
+    font-size: 1rem;
+    margin: 0.5rem 0;
+    color: #e1e1e6;
+    width: 100%;
+    padding: 0, 1.5rem;
+    height: 3.3rem;
+    outline: none;
+    padding: 1rem;
+
+    border-radius: 0.375rem;
+    border: 1px solid var(--black);
+    background: var(--black);
+  }
+
+  button[type="submit"] {
+    margin-top: 8px;
+    color: white;
+    width: 100%;
+    height: 3.6rem;
+    padding: 0, 1.5rem;
+    background: #00875f;
+    border-radius: 0.375rem;
+    border: 1px solid #00875f;
+    transition: filter 0.2s;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 160%;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+  button {
+    color: #c4c4cc;
+    height: 4rem;
+    border: 1px solid #29292e;
+    border-radius: 0.375rem;
+
+    background: #29292e;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
